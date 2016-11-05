7e30161a3743fdb304971e2fe0e03f6989ab23ce
orcas <7555ic@gmail.com>
2016-11-06 07:53:40 +0800

    Still fixing

--- index.js
+++ index.js
@@ -223,7 +223,7 @@ app.post('/upload', upload.single('uploader'), function(req, res){
 });
 app.get('/search', function(req, res){
 	res.data.imgs = [];
-	console.log(req.body);
+	console.log(req.params);
 	var terms = req.body.searchterms.split(" ");
 	File.find({}, function(err, files){
 		for(var i = 0; i < files.length; i++){
