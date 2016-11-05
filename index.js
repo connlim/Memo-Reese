18686198274671a35109f49492235e2ce562a908
orcas <7555ic@gmail.com>
2016-11-06 02:55:59 +0800

    Fixing pic view

--- index.js
+++ index.js
@@ -179,13 +179,13 @@ app.post('/upload', upload.single('uploader'), function(req, res){
 						Event.findOne({"location.textual" : res[0].formattedAddress}, function(err, result){
 							if(!result){
 								var newEvent = new Event({
-										name : datetime.toDateString() + "@" + res[0].formattedAddress,
+										name : exifData.exif.DateTimeOriginal + "@" + res[0].formattedAddress,
 										location : {
 											textual : res[0].formattedAddress,
 											lat : lat,
 											lng : lng
 										},
-										datetime : datetime
+										datetime : exifData.exif.DateTimeOriginal
 									});
 									console.log(newEvent);
 									newEvent.save();
